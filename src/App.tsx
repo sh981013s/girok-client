e33b240f8d57fa1f8c0c03b78c9d04d60d1fe520
sh981013s <sh981013s@gmail.com>
2023-10-02 01:45:47 +0900

    feat: set up routes for main page and basic user auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignUpPage from './pages/signUpPage/SignUpPage.tsx';
+import MainPage from './pages/mainPage/MainPage.tsx';
+import ROUTES from './constants/routes.ts';
+import SignInPage from './pages/signInPage/SignInPage.tsx';
+
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.MAIN_PAGE} element={<MainPage />} />
+        <Route path={ROUTES.SIGN_UP} element={<SignUpPage />} />
+        <Route path={ROUTES.SIGN_IN} element={<SignInPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+const ROUTES = {
+  MAIN_PAGE: '/',
+  SIGN_UP: '/signup',
+  SIGN_IN: '/signin',
+};
+
+export default ROUTES;
