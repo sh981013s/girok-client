fc3e431e0c6db99a5ed508badd2d9ca24245d5a4
sh981013s <sh981013s@gmail.com>
2023-11-27 18:58:34 +0900

    feat: add color theme constants for UI consistency

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    darkGray: '#131313',
+    red: '#D42727',
+    gray0: '#F8F8F8',
+    gray1: '#F1F1F1',
+    gray2: '#CFCFCF',
+    gray3: '#959595',
+    gray4: '#686868',
+    gray5: '#595959',
+    white: '#FFFFFF',
+    black: '#000000',
+  },
+} as const;
+
+export default theme;
