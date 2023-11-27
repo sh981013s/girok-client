54347b3b518be29b334d1b71f5334c0346636fb0
sh981013s <sh981013s@gmail.com>
2023-11-27 22:39:06 +0900

    feat: implement globally defined styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import ROUTES from '@/constants/routes.ts';
 import MainPage from '@/pages/mainPage/MainPage.tsx';
 import SignUpPage from '@/pages/signUpPage/SignUpPage.tsx';
 import SignInPage from '@/pages/signInPage/SignInPage.tsx';
 import CalendarPage from '@/pages/calendarPage/CalendarPage.tsx';
+import PageLayout from '@/components/_common/pageLayout/PageLayout.tsx';
+import theme from '@/styles/theme.ts';
+import GlobalStyle from '@/styles/GlobalStyle.tsx';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={ROUTES.MAIN_PAGE} element={<MainPage />} />
-        <Route path={ROUTES.SIGN_UP} element={<SignUpPage />} />
-        <Route path={ROUTES.SIGN_IN} element={<SignInPage />} />
-        <Route path={ROUTES.CALENDAR} element={<CalendarPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <PageLayout>
+          <Routes>
+            <Route path={ROUTES.MAIN_PAGE} element={<MainPage />} />
+            <Route path={ROUTES.SIGN_UP} element={<SignUpPage />} />
+            <Route path={ROUTES.SIGN_IN} element={<SignInPage />} />
+            <Route
+              path={ROUTES.CALENDAR}
+              element={<CalendarPage />}
+            />
+          </Routes>
+        </PageLayout>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,126 @@
+import { createGlobalStyle } from 'styled-components';
+
+import font from '../assets/fonts/Pretendard-Regular.woff';
+
+import BREAK_POINTS from '@/constants/breakPoints.ts';
+
+const GlobalStyle = createGlobalStyle`
+  /*! minireset.css v0.0.6 | MIT License | github.com/jgthms/minireset.css */
+  html {
+    box-sizing: border-box
+  }
+
+  blockquote, body, dd, dl, dt, fieldset, figure, h1, h2, h3, h4, h5, h6, hr, html, iframe, legend, li, ol, p, pre, textarea, ul {
+    margin: 0;
+    padding: 0
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-size: 100%;
+    font-weight: 400
+  }
+
+  ul {
+    list-style: none
+  }
+
+  li {
+    list-style: none;
+  }
+
+  button, input, select {
+    margin: 0;
+    background-color: transparent;
+    border: none;
+  }
+
+  *, :after, :before {
+    box-sizing: inherit;
+  }
+
+  img, video {
+    max-width: 100%;
+    height: auto
+  }
+
+  iframe {
+    border: 0
+  }
+
+
+  table {
+    border-spacing: 0;
+    border-collapse: collapse
+  }
+
+  td, th {
+    padding: 0
+  }
+
+  /* global styles */
+
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    font-display: optional;
+    src: url(${font}) format('woff');
+    unicode-range: U+0020-007E;
+  }
+
+  :root {
+    font-size: 62.5%; /* 1rem = 10px for all devices */
+
+    @media (max-width: ${BREAK_POINTS.MOBILE}px) {
+      font-size: 62.5%; /* 1rem = 10px for mobile devices */
+    }
+
+    @media (max-width: ${BREAK_POINTS.TABLET}px) {
+      font-size: 62.5%; /* 1rem = 10px for tablet devices */
+    }
+
+    @media (min-width: ${BREAK_POINTS.DESKTOP}px) {
+      font-size: 62.5%; /* 1rem = 10px for desktop devices */
+    }
+  }
+
+  * {
+    scrollbar-width: none;
+    font-family: 'Pretendard-Regular' , sans-serif;
+    white-space: pre-wrap;
+
+
+    &::-webkit-scrollbar {
+      display: none; /* 크롬, 사파리, 오페라, 엣지 */
+    }
+  }
+
+  input {
+    padding:0 ;
+    border: none;
+    outline: none;
+  }
+
+  textarea {
+    resize: none;
+    background-color: transparent;
+    border: none;
+    outline: none;
+  }
+
+  abbr {
+    text-decoration: none;
+  }
+  
+  button, select {
+    cursor: pointer;
+  }
+  
+  a {
+    text-decoration: none;
+  }
+  
+  input, select {
+    background: inherit;
+  }
+`;
+
+export default GlobalStyle;
